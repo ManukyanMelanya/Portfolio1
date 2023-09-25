1119545bffb96856e6c9def9137261311bee1b03
ManukyanMelanya <forpython2023@gmail.com>
2023-09-26 00:45:13 +0400

    I made changes in views.py

--- Portfolio/myapp/urls.py
+++ Portfolio/myapp/urls.py
@@ -6,5 +6,6 @@ app_name = "myapp"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.ProgrammerDetailView.as_view(), name="details"),
+    path("programmer", views.add_programmer, name="add_programmer"),
    
 ]
